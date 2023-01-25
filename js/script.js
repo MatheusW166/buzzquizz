f3eb38fc1953f36a93bf180110ab92e4f76c8aa8
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-01-25 19:19:56 -0300

    exibir quizzes servidor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 import { getQuizzes } from "./api.js";
-
-getQuizzes().then((res) => console.log(res));
-
+let Quizzes = [];
+getQuizzes().then((res) => {
+    console.log(res);
+    Quizzes = res;
+    exibirQuizzes();
+})
 export {};
+function exibirQuizzes(){
+    const listaQuizzes = document.querySelector('.listaQuizzes');
+    listaQuizzes.innerHTML = '';
+    for(let cont=0; cont<Quizzes.length; cont++){
+        let template = `
+        <li class="QuizzListado">
+            <img class="imgQuizz" src="${Quizzes[cont].image}" alt="${Quizzes[cont].title}">
+            <label class="tituloQuizz">${Quizzes[cont].title}</label>
+        </li>`;
+        listaQuizzes.innerHTML += template;
+        
+        
+    }
+}
+
