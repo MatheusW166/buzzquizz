ec57f8adabcc22ea3b267e8c40b7c476b1149e75
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-26 11:37:53 -0300

    Transição para a tela do quizz ao clicar em um quizz listado

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 import { getQuizzes } from "./api.js";
+import { criarTelaQuizz } from "./telaQuizz.js";
+
 let Quizzes = [];
 getQuizzes().then((res) => {
-    console.log(res);
-    Quizzes = res;
-    exibirQuizzes();
-})
-export {};
-function exibirQuizzes(){
-    const listaQuizzes = document.querySelector('.listaQuizzes');
-    listaQuizzes.innerHTML = '';
-    for(let cont=0; cont<Quizzes.length; cont++){
-        let template = `
-        <li class="QuizzListado">
+  console.log(res);
+  Quizzes = res;
+  exibirQuizzes();
+});
+
+const abrirTelaQuizz = (quizz) => {
+  const outrasTelas = document.querySelectorAll(".tela:not(.quizz)");
+  criarTelaQuizz(quizz);
+  outrasTelas.forEach((tela) => (tela.style.display = "none"));
+  console.log(outrasTelas);
+};
+
+function exibirQuizzes() {
+  const listaQuizzes = document.querySelector(".listaQuizzes");
+  listaQuizzes.innerHTML = "";
+  for (let cont = 0; cont < Quizzes.length; cont++) {
+    let template = `
+        <li class="QuizzListado clicavel">
             <img class="imgQuizz" src="${Quizzes[cont].image}" alt="${Quizzes[cont].title}">
             <label class="tituloQuizz">${Quizzes[cont].title}</label>
         </li>`;
-        listaQuizzes.innerHTML += template;
-        
-        
-    }
+    listaQuizzes.innerHTML += template;
+  }
+
+  const quizzesListados = document.querySelectorAll(".QuizzListado");
+  quizzesListados.forEach((quizzListado, idx) => {
+    quizzListado.onclick = () => abrirTelaQuizz(Quizzes[idx]);
+  });
 }
 
+export {};
