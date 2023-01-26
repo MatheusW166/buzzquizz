abae71510c99365ebb6facff0b68894c872112c6
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-25 23:00:58 -0300

    Criando tela do quizz de forma dinâmica e embaralhando as alternativas

--- js/telaQuizz.js
+++ js/telaQuizz.js
@@ -1,11 +1,11 @@
 const quizzDeExemplo = {
   id: 17961,
-  title: "Título do quizz",
+  title: "Você gosta de pão com ovo?",
   image: "https://http.cat/411.jpg",
   questions: [
     {
       title: "Título da pergunta 1",
-      color: "#123456",
+      color: "#a16207",
       answers: [
         {
           text: "Texto da resposta 1",
@@ -21,7 +21,7 @@ const quizzDeExemplo = {
     },
     {
       title: "Título da pergunta 2",
-      color: "#123456",
+      color: "#166534",
       answers: [
         {
           text: "Texto da resposta 1",
@@ -37,7 +37,7 @@ const quizzDeExemplo = {
     },
     {
       title: "Título da pergunta 3",
-      color: "#123456",
+      color: "#6b21a8",
       answers: [
         {
           text: "Texto da resposta 1",
@@ -67,3 +67,76 @@ const quizzDeExemplo = {
     },
   ],
 };
+
+const STATES = {
+  respondida: "respondida",
+  marcada: "marcada",
+  correta: "correta",
+  errada: "errada",
+};
+
+window.marcarResposta = (alternativa) => {
+  const containerDaPergunta = alternativa.parentElement.parentElement;
+  if (containerDaPergunta.classList.contains(STATES.respondida)) {
+    return;
+  }
+  alternativa.classList.add(STATES.marcada);
+  containerDaPergunta.classList.add(STATES.respondida);
+};
+
+function criarLayoutAlternativa(alternativa) {
+  return `
+  <div onclick="marcarResposta(this)" class="alternativa ${
+    alternativa.isCorrectAnswer ? STATES.correta : STATES.errada
+  }">
+    <img
+      src="${alternativa.image}"
+      alt="${alternativa.image}"
+    />
+    <p>${alternativa.text}</p>
+  </div>
+  `;
+}
+
+function embaralharAlternativas(alternativas) {
+  alternativas.sort(() => Math.random() - 0.5);
+}
+
+function criarLayoutDaPergunta(pergunta) {
+  embaralharAlternativas(pergunta.answers);
+  return `
+  <article class="pergunta">
+  <div style="background-color:${pergunta.color};" class="pergunta-titulo">
+    <h3>${pergunta.title}</h3>
+  </div>
+  <div class="alternativas">
+    ${pergunta.answers.reduce(
+      (prev, curr) => prev + criarLayoutAlternativa(curr),
+      ""
+    )}
+  </div>
+  </article>
+  `;
+}
+
+function criarTitulo(quizz) {
+  return `
+  <div class="titulo-container">
+    <h2>${quizz.title}</h2>
+    <div class="overlay"></div>
+    <img src="${quizz.image}" alt="${quizz.image}" />
+  </div>
+  `;
+}
+
+function criarTelaQuizz(quizz) {
+  const titulo = criarTitulo(quizz);
+  const perguntas = quizz.questions.reduce(
+    (prev, curr) => prev + criarLayoutDaPergunta(curr),
+    ""
+  );
+  const telaQuizz = document.querySelector(".tela-quizz");
+  telaQuizz.innerHTML = titulo + perguntas;
+}
+
+criarTelaQuizz(quizzDeExemplo);
