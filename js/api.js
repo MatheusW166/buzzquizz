bdffc1890354a98b66afc16432a3f04faf149b2f
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-27 12:40:33 -0300

    Restante das funções que comunicam com a API

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 const baseUrl = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 
+// Retorna todos os quizzes
 async function getQuizzes() {
   try {
     const res = await axios.get(baseUrl + "/quizzes");
@@ -10,5 +11,52 @@ async function getQuizzes() {
   }
 }
 
-export { getQuizzes };
+// Retorna o quizz com o id passado
+async function getQuizzPorId(idQuizz) {
+  try {
+    const res = await axios.get(baseUrl + `/quizzes/${idQuizz}`);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
 
+// Salva o quizz no servidor (necessário salvar a 'key' com localStorage)
+async function criarQuizz(quizz) {
+  try {
+    const res = await axios.post(baseUrl + "/quizzes", quizz);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+// (Bônus) Apaga o quizz com o id passado
+async function apagarQuizz(idQuizz, key) {
+  try {
+    const res = await axios.delete(baseUrl + `/quizzes/${idQuizz}`, {
+      headers: { "Secret-Key": key },
+    });
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+// (Bônus) Atualiza o quizz com o id passado
+async function atualizarQuizz(idQuizz, novoQuizz, key) {
+  try {
+    const res = await axios.put(baseUrl + `/quizzes/${idQuizz}`, novoQuizz, {
+      headers: { "Secret-Key": key },
+    });
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+export { getQuizzes, getQuizzPorId, criarQuizz, apagarQuizz, atualizarQuizz };
