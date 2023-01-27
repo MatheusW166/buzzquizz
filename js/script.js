87291d443eabc1a346c390c2986edc641e42a6ec
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-01-27 17:59:31 -0300

    espacamento quiz do usuario consertado

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ getQuizzes().then((res) => {
 
 const abrirTelaQuizz = (elementoClicado) => {
   const quizz = Quizzes.filter((quizz) => quizz.id == elementoClicado.id)[0];
+  debugger;
   criarTelaQuizz(quizz);
   const outrasTelas = document.querySelectorAll(".tela:not(.quizz)");
   outrasTelas.forEach((tela) => (tela.style.display = "none"));
@@ -61,14 +62,14 @@ function pegarQuizesLocais() {
 }
 function exibirQuizzesLocais(QuizLocal) {
   const listaQuizzes = document.querySelector(".listaQuizzesLocais");
-
+  console.log(QuizLocal);
   let template = `
           <li class="QuizzListado clicavel">
               <img class="imgQuizz" src="${QuizLocal.image}" alt="${QuizLocal.title}">
               <label class="tituloQuizz">${QuizLocal.title}</label>
           </li>`;
   listaQuizzes.innerHTML += template;
-  
+ 
 }
 export {};
 
